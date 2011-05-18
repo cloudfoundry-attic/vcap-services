92ad6b1b01cbdb5535fbdf90f4ba925da7e2aed4
tangr1 <ruit@mozy.com>
2011-05-18 14:28:24 +0800

    delete unused require file

--- rabbit/lib/rabbit_service/rabbit_node.rb
+++ rabbit/lib/rabbit_service/rabbit_node.rb
@@ -2,7 +2,6 @@
 require "set"
 require "datamapper"
 require "uuidtools"
-require "amqp"
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', '..', 'base', 'lib')
 require 'base/node'
