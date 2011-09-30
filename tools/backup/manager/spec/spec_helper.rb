1644d52eb85bbe8b830346a50b78f8fea7e948eb
felixhoo <hhu@vmware.com>
2011-09-29 23:00:27 -0700

    clean shutdown of backup manager
    Change-Id: Ia835a2daebe0679cda4c9bda6b5ce84f588054e9

--- tools/backup/manager/spec/spec_helper.rb
+++ tools/backup/manager/spec/spec_helper.rb
@@ -12,6 +12,66 @@ require 'backup_manager/manager'
 
 class BackupManagerTests
 
+  def self.create_manager(dirname)
+    root = File.join(File.dirname(__FILE__), 'test_directories', dirname)
+    mgr = ManagerTest.new({
+      :logger => Logger.new(STDOUT),
+      :wakeup_interval => 3,
+      :root => '',
+      :rotation => {
+        :max_days => 7
+      },
+      :enable => true
+    })
+    mgr.root = root
+    mgr
+  end
+
+  class ManagerTest < VCAP::Services::Backup::Manager
+    alias_method :ori_run,:run
+    attr_accessor :root
+    attr_reader :exit_invoked
+
+    def initialize(options)
+      super(options)
+      @tasks = [MockRotator.new(self,options[:rotation])]
+      @exit_invoked = false
+    end
+
+    def exit
+      @exit_invoked = true
+      @logger.debug("exit is called")
+    end
+
+    def run
+      ori_run
+      raise "break the loop"
+    end
+
+    def time
+      Time.parse("2010-01-20 09:10:20 UTC").to_i
+    end
+  end
+
+  class MockRotator < VCAP::Services::Backup::Rotator
+    alias_method :ori_prune,:prune
+    alias_method :ori_retain,:retain
+
+    def prune(path, timestamp=nil)
+      sleep 2
+      @manager.logger.debug("Test prune #{path}")
+      ori_prune(path,timestamp)
+    end
+
+    def retain(path, timestamp)
+      sleep 2
+      @manager.logger.debug("Test retain #{path}")
+      ori_retain(path,timestamp)
+    end
+
+    def rmdashr(path)
+    end
+  end
 end
 
 require 'backup_manager/rotator'
@@ -19,24 +79,24 @@ require 'backup_manager/rotator'
 
 class BackupRotatorTests
 
-  CC_PORT=45678
+  CC_PORT = 45678
   MAX_DAYS = 7
 
-  @@root=''
-  @@complicated=false
+  @@root = ''
+  @@complicated = false
 
   def self.create_rotator(root_p,opts)
     logger = Logger.new(STDOUT)
     @@complicated = root_p == 'complicated' # special handling for this one...
     if @@complicated
-      @@root=File.join('/tmp','backup_spec','test_directories',root_p)
+      @@root = File.join('/tmp','backup_spec','test_directories',root_p)
       require 'spec/test_directories/complicated/populate'
       populate_complicated(@@root)
     else
       @@root = File.join(File.dirname(__FILE__), 'test_directories', root_p)
     end
     manager = MockManager.new(@@root, logger)
-    opts.merge!({:logger=>logger})
+    opts.merge!({:logger => logger})
 
     RotatorTester.new(manager, opts)
   end
@@ -45,6 +105,14 @@ class BackupRotatorTests
     if @@complicated
       FileUtils.rm_rf(@@root)
     end
+  end
+
+  def self.validate_retained(backup,threshold)
+    path = backup[0]
+    timestamp = backup[1]
+    return true if timestamp > threshold
+    all_backup = Dir.entries(File.absolute_path('..',path))-['.','..']
+    return all_backup.max.to_i == timestamp
   end
 
   class MockManager
@@ -56,6 +124,9 @@ class BackupRotatorTests
     def time
       Time.parse("2010-01-20 09:10:20 UTC").to_i
     end
+    def shutdown?
+      false
+    end
   end
 
   class RotatorTester < VCAP::Services::Backup::Rotator
@@ -63,7 +134,7 @@ class BackupRotatorTests
       super(manager, options)
       @pruned = []
       @retained = []
-      @logger=options[:logger]
+      @logger = options[:logger]
     end
     def prune(path, timestamp=nil)
       # IMPORTANT: by overriding this method we prevent files in
@@ -103,7 +174,7 @@ class BackupRotatorTests
       get "/services/v1/offerings/:label/handles" do
         case params['label'].gsub!(/-.*$/,'')
         when 'mysql'
-          res=Yajl::Encoder.encode({
+          res = Yajl::Encoder.encode({
             :handles => [{
             'service_id' => 'd35b51e7814b34eeeb9bbb3a6b8750755',
             'configuration' => {},
@@ -111,7 +182,7 @@ class BackupRotatorTests
           }]
           })
         when 'redis'
-          res=Yajl::Encoder.encode({
+          res = Yajl::Encoder.encode({
             :handles => [{
             'service_id' => 'f0fe7695-0310-4043-be12-0a09e59e52d0',
             'configuration' => {},
@@ -119,7 +190,7 @@ class BackupRotatorTests
           }]
           })
         when 'mongodb'
-          res=Yajl::Encoder.encode({
+          res = Yajl::Encoder.encode({
             :handles => [{
             'service_id' => 'a7d3b56a-92b4-4e70-8efe-080ae129f83b',
             'configuration' => {},
@@ -127,21 +198,10 @@ class BackupRotatorTests
           }]
           })
         else
-          res='{}'
+          res = '{}'
         end
         res
       end
     end
   end
 end
-
-def validate_retained(backup,threshold)
-  path=backup[0]
-  timestamp=backup[1]
-  return true if timestamp>threshold
-  all_backup=Dir.entries(File.absolute_path('..',path))-['.','..']
-  return all_backup.max.to_i==timestamp
-end
-
-
-
