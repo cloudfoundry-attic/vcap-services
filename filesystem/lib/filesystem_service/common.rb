a9e0b0078f5929aef4e4ffe9cb605855078e4206
Tang Rui <rtang@vmware.com>
2011-11-22 19:37:14 -0800

    Add filesystem service
    Change-Id: Ic3be2e362c89ae36e05a0ba6d4678e20333acb5c

---
+++ filesystem/lib/filesystem_service/common.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+module VCAP
+  module Services
+    module Filesystem
+      module Common
+        def service_name
+          "FilesystemaaS"
+        end
+      end
+    end
+  end
+end
