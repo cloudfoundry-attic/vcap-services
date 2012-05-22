39d4eb9f1b3417852606c719c0bddb803f736e23
Chunjie <czhu@vmware.com>
2012-05-21 22:10:08 -0700

    fix service error code number bug
    Change-Id: Ie8c03cd323c271dc5fb9aee4cf3c9984e62665fe

--- postgresql/lib/postgresql_service/postgresql_error.rb
+++ postgresql/lib/postgresql_service/postgresql_error.rb
@@ -2,10 +2,10 @@
 
 class VCAP::Services::Postgresql::PostgresqlError<
   VCAP::Services::Base::Error::ServiceError
-    POSTGRESQL_DISK_FULL = [32001, HTTP_INTERNAL, 'Node disk is full.']
-    POSTGRESQL_CONFIG_NOT_FOUND = [32002, HTTP_NOT_FOUND, 'Postgresql configuration %s not found.']
-    POSTGRESQL_CRED_NOT_FOUND = [32003, HTTP_NOT_FOUND, 'Postgresql credential %s not found.']
-    POSTGRESQL_LOCAL_DB_ERROR = [32004, HTTP_INTERNAL, 'Postgresql node local db error.']
-    POSTGRESQL_INVALID_PLAN = [32005, HTTP_INTERNAL, 'Invalid plan %s.']
-    POSTGRESQL_BAD_SERIALIZED_DATA = [32007, HTTP_INTERNAL, 'Invalid serialized data.']
+    POSTGRESQL_DISK_FULL = [31801, HTTP_INTERNAL, 'Node disk is full.']
+    POSTGRESQL_CONFIG_NOT_FOUND = [31802, HTTP_NOT_FOUND, 'Postgresql configuration %s not found.']
+    POSTGRESQL_CRED_NOT_FOUND = [31803, HTTP_NOT_FOUND, 'Postgresql credential %s not found.']
+    POSTGRESQL_LOCAL_DB_ERROR = [31804, HTTP_INTERNAL, 'Postgresql node local db error.']
+    POSTGRESQL_INVALID_PLAN = [31805, HTTP_INTERNAL, 'Invalid plan %s.']
+    POSTGRESQL_BAD_SERIALIZED_DATA = [31807, HTTP_INTERNAL, 'Invalid serialized data.']
 end
