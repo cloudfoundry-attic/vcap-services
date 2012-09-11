88c8ac77be151feb2664e6293f0a33b7a16a4f20
Yang Yang <yangy@vmware.com>
2012-09-11 02:11:28 -0700

    fix a bug in vblob migration unit test; avoid hard-codes in some unit-test
    Change-Id: I8b01c36d562b99751badb2023692af4d8a9d4305

--- vblob/spec/vblob_migration_spec.rb
+++ vblob/spec/vblob_migration_spec.rb
@@ -4,18 +4,18 @@ $:.unshift(File.dirname(__FILE__))
 
 require "spec_helper"
 
-describe "vblob_node exception test" do
+describe "vblob_node migration test" do
 
   before :all do
     EM.run do
       @opts = get_node_config()
       @logger = @opts[:logger]
       @node = Node.new(@opts)
-      @response = @node.provision("free")
-      @provisioned_service = @node.get_instance(@response['name'])
-      @bind_response = @node.bind(@response['name'], 'rw')
       EM.add_timer(1) { EM.stop }
     end
+    @response = @node.provision("free")
+    @provisioned_service = @node.get_instance(@response['name'])
+    @bind_response = @node.bind(@response['name'], 'rw')
   end
 
   after :all do
@@ -27,13 +27,14 @@ describe "vblob_node exception test" do
   it "should be able to disable the instance" do
     @node.disable_instance(@response, {'' => {'credentials' => ''}}).should be_true
     is_port_open?(@provisioned_service.ip, @provisioned_service.service_port).should be_false
-  end
-
-  it "should be able to return the failed healthz during the disabled period" do
     @node.get_healthz(@provisioned_service).should == "fail"
+    @node.is_service_started(@provisioned_service).should == false
   end
 
-  it "should be able to raise vblob_start_instance_error when it is diabled" do
-    @node.is_service_started(@provisioned_service).should == false
+  it "should be able to enable the instance after diabling it" do
+    @node.disable_instance(@response, {'' => {'credentials' => ''}}).should be_true
+    @node.enable_instance(@response, {'' => {'credentials' => ''}}).should be_true
+    @provisioned_service = @node.get_instance(@response['name'])
+    is_port_open?(@provisioned_service.ip, @provisioned_service.service_port).should be_true
   end
 end
