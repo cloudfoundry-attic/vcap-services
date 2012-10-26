b92ff727511584aec59e2a19b5650f88d53a4860
Chunjie <czhu@vmware.com>
2012-10-26 03:10:06 -0700

    bring ring buffer to avoid extra memory copy operations
    Change-Id: Ib406dcb56c32cf9d313c4e4b3310a094aaae79e2

--- tools/mongodb_proxy/src/go-mongo-proxy/proxy/netio.go
+++ tools/mongodb_proxy/src/go-mongo-proxy/proxy/netio.go
@@ -12,26 +12,31 @@ const FILTER_BLOCK = -4
 const UNKNOWN_ERROR = -1024
 
 const MAX_LISTEN_BACKLOG = 100
-const BUFFER_SIZE = 512 // TODO: buffer size tuning ???
+const BUFFER_SIZE = 1024 * 1024 // 1M buffer size
 
 type IOSocketPeer struct {
 	clientfd int // TCP connection with mongo client
 	serverfd int // TCP connection with mongo server
 
-	conninfo syscall.Sockaddr // conection peer
+	conninfo syscall.Sockaddr // conection
 
 	recvpacket func(*NetIOManager, int) int
 	sendpacket func(*NetIOManager, int) int
 }
 
 type OutputQueue struct {
-	packet []byte
-	stream []byte
+	// current mongo client -> server packets
+	current_packet_op            int32
+	current_packet_remain_length int32
+	// ring buffer
+	write_offset   int32
+	read_offset    int32
+	available_size int32
+	stream         []byte
 }
 
 type NetIOManager struct {
 	max_backlog         int
-	skb                 []byte
 	io_socket_peers     map[int]*IOSocketPeer
 	pending_output_skbs map[int]*OutputQueue
 	epoll_fd            int
@@ -42,7 +47,6 @@ type NetIOManager struct {
 func NewNetIOManager() *NetIOManager {
 	io_manager := &NetIOManager{
 		max_backlog:         MAX_LISTEN_BACKLOG,
-		skb:                 make([]byte, BUFFER_SIZE),
 		io_socket_peers:     make(map[int]*IOSocketPeer),
 		pending_output_skbs: make(map[int]*OutputQueue),
 		epoll_fd:            -1,
@@ -151,6 +155,10 @@ func (io *NetIOManager) ProxyNetAccept(serverinfo syscall.Sockaddr) (sa syscall.
 
 	// now proxy server becomes a bridge between client <-> server
 	add_sock_peer(io, clientfd, clientinfo, serverfd, serverinfo)
+	// client -> server channel buffer
+	alloc_skb_buffer(io, clientfd)
+	// server -> client channel buffer
+	alloc_skb_buffer(io, serverfd)
 	return clientinfo, nil
 
 ServerCleanup:
@@ -216,171 +224,243 @@ func (io *NetIOManager) ProxyNetRecv(fd int) (errno int) {
 /*    network read/write io routines      */
 /*                                        */
 /******************************************/
-
 func skb_read(io *NetIOManager, fd int) (errno int) {
-	for {
-		num, err := syscall.Read(fd, io.skb)
+	var peerfd int
 
-		if num < 0 && err != nil {
-			if err == syscall.EAGAIN {
-				return NO_ERROR
-			} else if err == syscall.EWOULDBLOCK {
-				return NO_ERROR
-			} else if err == syscall.EINTR {
-				// TODO: anything to do??? retry???
-				return READ_ERROR
-			} else {
-				return READ_ERROR
-			}
-		} else if num == 0 {
-			return SESSION_EOF
+	if peers, ok := io.io_socket_peers[fd]; ok {
+		if fd == peers.clientfd {
+			peerfd = peers.serverfd
 		} else {
-			// append skb into peer fd's output queue
-			var peerfd int
-			if fd == io.io_socket_peers[fd].clientfd {
-				peerfd = io.io_socket_peers[fd].serverfd
-			} else {
-				peerfd = io.io_socket_peers[fd].clientfd
-			}
-			skb_enqueue_output(io, peerfd, io.skb[0:num])
+			peerfd = peers.clientfd
+		}
+	} else {
+		return UNKNOWN_ERROR
+	}
 
-			if num < len(io.skb) {
-				break
+	if pending, ok := io.pending_output_skbs[peerfd]; ok {
+		if pending.available_size <= 0 {
+			return NO_ERROR
+		}
+
+		// determine the maximum size which can be recv
+		start_offset := pending.write_offset
+		end_offset := start_offset + pending.available_size
+		if end_offset > BUFFER_SIZE {
+			end_offset = BUFFER_SIZE
+		}
+		num, error := do_skb_read(fd, pending.stream[start_offset:end_offset])
+		if (error == NO_ERROR) && (num > 0) {
+			pending.write_offset = (start_offset + num) % BUFFER_SIZE
+			pending.available_size -= num
+			if num < (end_offset - start_offset) {
+				return NO_ERROR
 			}
+		} else {
+			return error
+		}
+
+		if pending.available_size <= 0 {
+			return NO_ERROR
+		}
+
+		// wrap around the ring buffer
+		start_offset = pending.write_offset
+		end_offset = start_offset + pending.available_size
+		num, error = do_skb_read(fd, pending.stream[start_offset:end_offset])
+		if (error == NO_ERROR) && (num > 0) {
+			pending.write_offset = start_offset + num
+			pending.available_size -= num
+			return NO_ERROR
+		} else {
+			return error
 		}
 	}
-	return NO_ERROR
+	return UNKNOWN_ERROR
 }
 
 func skb_write_with_filter(io *NetIOManager, fd int) (errno int) {
-	nwrite := 0
 	if pending, ok := io.pending_output_skbs[fd]; ok {
-		if len(pending.packet) > 0 {
-			/*
-			 * NOTE: We must get mongodb protocol packet one-by-one from the
-			 *       output queue, then we can parse the packet header to
-			 *       block insert/update operations if need.
-			 */
-			nwrite, err := syscall.Write(fd, pending.packet)
-			if nwrite < 0 && err != nil {
-				if err == syscall.EAGAIN {
+		for {
+			if pending.current_packet_remain_length == 0 {
+				var packet_header []byte
+
+				if BUFFER_SIZE-pending.available_size < STANDARD_HEADER_SIZE {
 					return NO_ERROR
-				} else if err == syscall.EWOULDBLOCK {
-					return NO_ERROR
-				} else if err == syscall.EINTR {
-					//TODO: anything to do??? retry???
-					return WRITE_ERROR
-				} else {
-					return WRITE_ERROR
 				}
-			} else if nwrite == 0 {
-				return NO_ERROR
-			} else {
-				io.pending_output_skbs[fd].packet = pending.packet[nwrite:len(pending.packet)]
-				if len(io.pending_output_skbs[fd].packet) > 0 {
-					return NO_ERROR
+
+				start_offset := pending.read_offset
+				end_offset := pending.read_offset + BUFFER_SIZE - pending.available_size
+				if end_offset > BUFFER_SIZE {
+					end_offset = BUFFER_SIZE
+				}
+
+				// determine whether the left size is larger than STANDARD_HEADER_SIZE
+				if end_offset-start_offset >= STANDARD_HEADER_SIZE {
+					packet_header = pending.stream[start_offset:(start_offset + STANDARD_HEADER_SIZE)]
+				} else {
+					// wrap around the ring buffer
+					packet_header = make([]byte, STANDARD_HEADER_SIZE)
+					copy(packet_header, pending.stream[start_offset:end_offset])
+					copy(packet_header[end_offset:], pending.stream[0:(STANDARD_HEADER_SIZE+start_offset-end_offset)])
+				}
+
+				/*
+				 * NOTE: We must get mongodb protocol packet one-by-one from the
+				 *       ring buffer, then we can parse the packet header to
+				 *       block insert/update operations if need.
+				 */
+				message_length, op_code := io.filter.HandleMsgHeader(packet_header)
+				if message_length > 0 {
+					pending.current_packet_op = op_code
+					pending.current_packet_remain_length = message_length
+				} else {
+					pending.current_packet_op = OP_UNKNOWN
+					pending.current_packet_remain_length = 0
 				}
 			}
-		}
 
-		for {
-			message_length, op_code := io.filter.HandleMsgHeader(
-				io.pending_output_skbs[fd].stream)
-			if message_length > 0 {
-				if !io.filter.PassFilter(op_code) {
-					// block operation
-					return FILTER_BLOCK
-				}
+			if !io.filter.PassFilter(pending.current_packet_op) {
+				// block operation
+				return FILTER_BLOCK
+			}
 
-				num, err := syscall.Write(fd, io.pending_output_skbs[fd].
-					stream[0:message_length])
-				if num < 0 && err != nil {
-					if err == syscall.EAGAIN {
-						return NO_ERROR
-					} else if err == syscall.EWOULDBLOCK {
-						return NO_ERROR
-					} else if err == syscall.EINTR {
-						//TODO: anything to do??? retry???
-						return WRITE_ERROR
-					} else {
-						return WRITE_ERROR
-					}
-				} else if num == 0 {
+			// Figure out real buffered data size
+			sendlen := pending.current_packet_remain_length
+			if BUFFER_SIZE-pending.available_size < sendlen {
+				sendlen = BUFFER_SIZE - pending.available_size
+			}
+
+			/*
+			 * NOTE: Do not enter into 'write' system call if there is nothing
+			 *       to transmit, system call is expensive.
+			 */
+			if sendlen <= 0 {
+				return NO_ERROR
+			}
+
+			// determine the maximum size which can be send
+			start_offset := pending.read_offset
+			end_offset := pending.read_offset + sendlen
+			if end_offset > BUFFER_SIZE {
+				end_offset = BUFFER_SIZE
+			}
+			num, error := do_skb_write(fd, pending.stream[start_offset:end_offset])
+			if (error == NO_ERROR) && (num > 0) {
+				pending.read_offset = (start_offset + num) % BUFFER_SIZE
+				pending.available_size += num
+				sendlen -= num
+
+				pending.current_packet_remain_length -= num
+
+				if num < (end_offset - start_offset) {
 					return NO_ERROR
-				} else {
-					nwrite += num
-					skb_dequeue_output(io, fd, int(message_length))
-					if num < int(message_length) {
-						add_partial_skb(io, fd, io.pending_output_skbs[fd].
-							stream[num:message_length])
-						return NO_ERROR
-					}
 				}
 			} else {
-				break
+				return error
+			}
+
+			if sendlen > 0 {
+				// wrap around the ring buffer
+				start_offset = pending.read_offset
+				end_offset = start_offset + sendlen
+				num, error = do_skb_write(fd, pending.stream[start_offset:end_offset])
+				if (error == NO_ERROR) && (num > 0) {
+					pending.read_offset = start_offset + num
+					pending.available_size += num
+
+					pending.current_packet_remain_length -= num
+				} else {
+					return error
+				}
 			}
 		}
 	}
-	return NO_ERROR
+	return UNKNOWN_ERROR
 }
 
 func skb_write_without_filter(io *NetIOManager, fd int) (errno int) {
 	if pending, ok := io.pending_output_skbs[fd]; ok {
-		num, err := syscall.Write(fd, pending.stream)
-		if num < 0 && err != nil {
-			if err == syscall.EAGAIN {
-				return NO_ERROR
-			} else if err == syscall.EWOULDBLOCK {
-				return NO_ERROR
-			} else if err == syscall.EINTR {
-				//TODO: anything to do??? retry???
-				return WRITE_ERROR
-			} else {
-				return WRITE_ERROR
-			}
-		} else if num == 0 {
-			return NO_ERROR
-		} else {
-			skb_dequeue_output(io, fd, num)
+		if BUFFER_SIZE-pending.available_size <= 0 {
 			return NO_ERROR
 		}
+
+		// determine the maximum size which can be send
+		start_offset := pending.read_offset
+		end_offset := pending.read_offset + BUFFER_SIZE - pending.available_size
+		if end_offset > BUFFER_SIZE {
+			end_offset = BUFFER_SIZE
+		}
+		num, error := do_skb_write(fd, pending.stream[start_offset:end_offset])
+		if (error == NO_ERROR) && (num > 0) {
+			pending.read_offset = (start_offset + num) % BUFFER_SIZE
+			pending.available_size += num
+			if num < (end_offset - start_offset) {
+				return NO_ERROR
+			}
+		} else {
+			return error
+		}
+
+		if BUFFER_SIZE-pending.available_size <= 0 {
+			return NO_ERROR
+		}
+
+		// wrap around the ring buffer
+		start_offset = pending.read_offset
+		end_offset = start_offset + BUFFER_SIZE - pending.available_size
+		num, error = do_skb_write(fd, pending.stream[start_offset:end_offset])
+		if (error == NO_ERROR) && (num > 0) {
+			pending.read_offset = start_offset + num
+			pending.available_size += num
+			return NO_ERROR
+		} else {
+			return error
+		}
 	}
-	return NO_ERROR
+	return UNKNOWN_ERROR
 }
 
 /******************************************/
 /*                                        */
-/*       Internel Support Routines        */
+/*       Internal Support Routines        */
 /*                                        */
 /******************************************/
-
-func skb_enqueue_output(io *NetIOManager, fd int, data []byte) {
-	if pending, ok := io.pending_output_skbs[fd]; ok {
-		io.pending_output_skbs[fd].stream = append(pending.stream, data)
-	} else {
-		io.pending_output_skbs[fd] = &OutputQueue{make([]byte, 0), data}
+func do_skb_read(fd int, data []byte) (num int32, errno int) {
+	nread, err := syscall.Read(fd, data)
+	if nread < 0 && err != nil {
+		if err == syscall.EAGAIN {
+			return 0, NO_ERROR
+		} else if err == syscall.EWOULDBLOCK {
+			return 0, NO_ERROR
+		} else if err == syscall.EINTR {
+			// TODO: anything to do??? retry???
+			return 0, READ_ERROR
+		} else {
+			return 0, READ_ERROR
+		}
+	} else if nread == 0 {
+		return 0, SESSION_EOF
 	}
+	return int32(nread), NO_ERROR
 }
 
-func skb_dequeue_output(io *NetIOManager, fd int, num int) {
-	if pending, ok := io.pending_output_skbs[fd]; ok {
-		io.pending_output_skbs[fd].stream = pending.stream[num:]
+func do_skb_write(fd int, data []byte) (num int32, errno int) {
+	nwrite, err := syscall.Write(fd, data)
+	if nwrite < 0 && err != nil {
+		if err == syscall.EAGAIN {
+			return 0, NO_ERROR
+		} else if err == syscall.EWOULDBLOCK {
+			return 0, NO_ERROR
+		} else if err == syscall.EINTR {
+			//TODO: anything to do??? retry???
+			return 0, WRITE_ERROR
+		} else {
+			return 0, WRITE_ERROR
+		}
+	} else if nwrite == 0 {
+		return 0, NO_ERROR
 	}
-}
-
-func add_partial_skb(io *NetIOManager, fd int, data []byte) {
-	if _, ok := io.pending_output_skbs[fd]; ok {
-		io.pending_output_skbs[fd].packet = data
-	} else {
-		io.pending_output_skbs[fd] = &OutputQueue{data, make([]byte, 0)}
-	}
-}
-
-func append(skb1, skb2 []byte) (skb []byte) {
-	newskb := make([]byte, len(skb1)+len(skb2))
-	copy(newskb, skb1)
-	copy(newskb[len(skb1):], skb2)
-	return newskb
+	return int32(nwrite), NO_ERROR
 }
 
 func add_sock_peer(io *NetIOManager,
@@ -392,16 +472,27 @@ func add_sock_peer(io *NetIOManager,
 		 * NOTE: We only filter request from mongo client to mongo server
 		 *       when filter is enabled.
 		 */
-		server_peer = IOSocketPeer{clientfd, serverfd, clientinfo, skb_read,
+		server_peer = IOSocketPeer{clientfd, serverfd, serverinfo, skb_read,
 			skb_write_with_filter}
 	} else {
-		server_peer = IOSocketPeer{clientfd, serverfd, clientinfo, skb_read,
+		server_peer = IOSocketPeer{clientfd, serverfd, serverinfo, skb_read,
 			skb_write_without_filter}
 	}
-	client_peer := IOSocketPeer{clientfd, serverfd, serverinfo, skb_read,
+	client_peer := IOSocketPeer{clientfd, serverfd, clientinfo, skb_read,
 		skb_write_without_filter}
 	io.io_socket_peers[clientfd] = &client_peer
 	io.io_socket_peers[serverfd] = &server_peer
+}
+
+func alloc_skb_buffer(io *NetIOManager, fd int) {
+	io.pending_output_skbs[fd] = &OutputQueue{
+		current_packet_op:            OP_UNKNOWN,
+		current_packet_remain_length: 0,
+		write_offset:                 0,
+		read_offset:                  0,
+		available_size:               BUFFER_SIZE,
+		stream:                       make([]byte, BUFFER_SIZE),
+	}
 }
 
 func sock_close(io *NetIOManager, fd int) {
