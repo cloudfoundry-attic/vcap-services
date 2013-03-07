749bc2bf75d5376f23c6f81301372a8b9ca35e1a
Matt Royal and Rui Tang <pair+mroyal+rui@pivotallabs.com>
2013-03-07 11:41:00 -0800

    Pull down binaries from S3 before running tests
    [#45783737]

--- ci_build.sh
+++ ci_build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 set -e
-sudo mkdir -p /var/vcap
-sudo chown $USER /var/vcap
-cd $FOLDER_NAME && bundle install --deployment && bundle exec rake spec --trace
+sudo mkdir -p /var/vcap/packages
+sudo mkdir -p /var/vcap/store
+sudo chown -R $USER /var/vcap
+./download_binaries_from_s3.rb
+tar xf common.tar.gz -C /
+tar xf packages.tar.gz -C /
+rm -f common.tar.gz packages.tar.gz
+cd $FOLDER_NAME && bundle install --local && bundle exec rake spec --trace
