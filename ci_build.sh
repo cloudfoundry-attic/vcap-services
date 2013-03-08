424232881970c38955807dddf6e83d7d37c7048d
Phan Le and Rui Tang <pair+phan+rui@pivotallabs.com>
2013-03-08 11:44:06 -0800

    Always install aws-sdk gem

--- ci_build.sh
+++ ci_build.sh
@@ -3,13 +3,13 @@ set -e
 sudo mkdir -p /var/vcap/packages
 sudo mkdir -p /var/vcap/store
 sudo chown -R $USER /var/vcap
+sudo gem install --no-ri --no-rdoc aws-sdk
 if [ $WARDENIZED_SERVICE ]
 then
   ./start_warden.sh
 fi
 if [ $REQUIRE_PACKAGE ]
 then
-  sudo gem install --no-ri --no-rdoc aws-sdk
   ./download_binaries_from_s3.rb $REQUIRE_PACKAGE
   tar xf $REQUIRE_PACKAGE -C /
   rm -f $REQUIRE_PACKAGE
