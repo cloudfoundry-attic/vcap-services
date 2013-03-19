de3afc52842467a118d093ff129f742ba49bc406
Andrew Liu and David Stevenson <pair+aliu+stevend@pivotallabs.com>
2013-03-19 10:01:21 -0700

    Move community supported services to cf-services-contrib repository

--- couchdb/lib/couchdb_service/couchdb_provisioner.rb
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2009-2011 VMware, Inc.
-require "couchdb_service/common"
-
-class VCAP::Services::CouchDB::Provisioner < VCAP::Services::Base::Provisioner
-
-  include VCAP::Services::CouchDB::Common
-
-end
-
--- elasticsearch/lib/elasticsearch_service/elasticsearch_provisioner.rb
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2009-2011 VMware, Inc.
-require "elasticsearch_service/common"
-
-class VCAP::Services::ElasticSearch::Provisioner < VCAP::Services::Base::Provisioner
-
-  include VCAP::Services::ElasticSearch::Common
-
-end
