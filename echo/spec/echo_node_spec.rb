d83c56358f1488fa415ab843c6d669f5cb01300e
Matt Royal and Phan Le <pair+mroyal+phan@pivotallabs.com>
2013-03-25 09:35:29 -0700

    Fix the test

--- echo/spec/echo_node_spec.rb
+++ echo/spec/echo_node_spec.rb
@@ -18,68 +18,68 @@ module VCAP
   module Services
     module Echo
       class EchoError
-          attr_reader :error_code
+        attr_reader :error_code
       end
     end
   end
 end
 
-describe "Echo service node" do
-  include VCAP::Services::Echo
-
-  before :all do
-    @opts = get_node_test_config
-    @opts.freeze
-    @logger = @opts[:logger]
-    # Setup code must be wrapped in EM.run
-    EM.run do
-      @node = Node.new(@opts)
-      EM.add_timer(1) { EM.stop }
+module VCAP::Services::Echo
+  describe "Echo service node" do
+    before :all do
+      @opts = get_node_test_config
+      @opts.freeze
+      @logger = @opts[:logger]
+      # Setup code must be wrapped in EM.run
+      EM.run do
+        @node = Node.new(@opts)
+        EM.add_timer(1) { EM.stop }
+      end
     end
-  end
 
-  before :each do
-    @default_plan = "free"
-    @default_opts = "default"
-    @echoer = @node.provision(@default_plan)
-    @echoer.should_not == nil
-  end
-
-  it "should provison a echo service with correct credential" do
-    EM.run do
-      @echoer.should be_instance_of Hash
-      @echoer["port"].should be 5002
-      EM.stop
+    before :each do
+      @default_plan = "free"
+      @default_opts = "default"
+      @echoer = @node.provision(@default_plan)
+      @echoer.should_not == nil
     end
-  end
 
-  it "should create a crediential when binding" do
-    EM.run do
-      binding = @node.bind(@echoer["name"], @default_opts)
-      binding["port"].should be 5002
-      EM.stop
+    it "should provison a echo service with correct credential" do
+      EM.run do
+        @echoer.should be_instance_of Hash
+        @echoer["port"].should be 5002
+        EM.stop
+      end
     end
-  end
 
-  it "should supply different credentials when binding evoked with the same input" do
-    EM.run do
-      binding1 = @node.bind(@echoer["name"], @default_opts)
-      binding2 = @node.bind(@echoer["name"], @default_opts)
-      binding1.should_not be binding2
-      EM.stop
+    it "should create a crediential when binding" do
+      EM.run do
+        binding = @node.bind(@echoer["name"], @default_opts)
+        binding["port"].should be 5002
+        EM.stop
+      end
     end
-  end
 
-  it "shoulde delete crediential after unbinding" do
-    EM.run do
-      binding = @node.bind(@echoer["name"], @default_opts)
-      @node.unbind(binding)
-      EM.stop
+    it "should supply different credentials when binding evoked with the same input" do
+      EM.run do
+        binding1 = @node.bind(@echoer["name"], @default_opts)
+        binding2 = @node.bind(@echoer["name"], @default_opts)
+        binding1.should_not be binding2
+        EM.stop
+      end
     end
-  end
 
-  after :each do
-    name = @echoer["name"]
-    @node.unprovision(name)
+    it "shoulde delete crediential after unbinding" do
+      EM.run do
+        binding = @node.bind(@echoer["name"], @default_opts)
+        @node.unbind(binding)
+        EM.stop
+      end
+    end
+
+    after :each do
+      name = @echoer["name"]
+      @node.unprovision(name)
+    end
   end
 end
