410a95ea88e7342982c202d6269fbedbe5b6b2fb
David Stevenson and Ryan Tang <pair+stevend+ryantang@pivotallabs.com>
2013-04-01 09:44:56 -0700

    Rabbit data migration has default values

--- ng/rabbit/lib/rabbit_service/rabbit_node.rb
+++ ng/rabbit/lib/rabbit_service/rabbit_node.rb
@@ -335,8 +335,8 @@ class VCAP::Services::Rabbit::Node::ProvisionedService
   property :admin_port,      Integer,     :unique => true
   property :admin_username,  String,      :required => true
   property :admin_password,  String,      :required => true
-  property :monit_username,  String,      :required => true
-  property :monit_password,  String,      :required => true
+  property :monit_username,  String,      :required => true, :default => "none"
+  property :monit_password,  String,      :required => true, :default => "none"
   # property plan is deprecated. The instances in one node have same plan.
   property :plan,            Integer,     :required => true
   property :plan_option,     String,      :required => false
