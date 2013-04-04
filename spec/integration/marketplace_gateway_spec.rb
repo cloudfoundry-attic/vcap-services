774c9baeec0341529574a80e1a373598eb74c7ba
David Stevenson and Matt Royal <pair+stevend+mroyal@pivotallabs.com>
2013-04-04 15:28:07 -0700

    Fix integration test due to AD data change

--- spec/integration/marketplace_gateway_spec.rb
+++ spec/integration/marketplace_gateway_spec.rb
@@ -4,14 +4,14 @@ describe 'Marketplace Gateway - AppDirect integration' do
   it 'populates CC with AppDirect services', components: [:ccng, :marketplace]  do
     services_response = nil
     # retry a few times, since the service may not advertise offerings immediately
-    5.times do
+    10.times do
       services_response = ccng_get('/v2/services')
       break if services_response.fetch('resources').any?
       sleep 0.5
     end
     services_response.fetch('resources').should have(1).entry
     entity = services_response.fetch('resources').first.fetch('entity')
-    entity.fetch('label').should == 'smtp'
-    entity.fetch('provider').should == 'sendgrid'
+    entity.fetch('label').should == 'mongodb'
+    entity.fetch('provider').should == 'mongolab'
   end
 end
