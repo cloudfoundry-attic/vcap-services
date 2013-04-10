f59f3574b1ebf8429604472eaedc5020cd4a790f
Phan Le and Scott Calvert <pair+phan+scalvert@pivotallabs.com>
2013-04-10 10:00:57 -0700

    Removed references to ExtraInformationFetcher

--- marketplace/spec/appdirect/app_direct_catalog_spec.rb
+++ marketplace/spec/appdirect/app_direct_catalog_spec.rb
@@ -4,7 +4,6 @@ require "marketplaces/appdirect/app_direct_catalog"
 module VCAP::Services::Marketplace::Appdirect
   describe AppDirectCatalog do
     describe "#current_offering" do
-      let(:fetcher) { double('ExtraInformationFetcher') }
       let(:client) { ->(*_) { [200, response_body] } }
       let(:filter_response) { double('filter_response').as_null_object }
       let(:filter) { double('filter', filter: filter_response) }
@@ -49,15 +48,6 @@ module VCAP::Services::Marketplace::Appdirect
           offerings.should have(1).items
           offerings.first.label.should == "mongodb"
           offerings.first.provider.should == "mongolab-dev"
-        end
-
-        it "integrates extra data" do
-          ExtraInformationFetcher.stub(:new).and_return(fetcher)
-
-          offerings_with_extra_info = double('offerings with extra info')
-          fetcher.should_receive(:fetch_extra_information).and_return(offerings_with_extra_info)
-
-          catalog.current_offerings(filter).should == offerings_with_extra_info
         end
       end
     end
