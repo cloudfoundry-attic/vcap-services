aeee61bc80a9cad13f861d5f9071255ea87ce026
David Stevenson and Scott Calvert <pair+stevend+scalvert@pivotallabs.com>
2013-04-11 11:49:37 -0700

    Backfill integration test for mysql bind

--- spec/integration/mysql_shared_spec.rb
+++ spec/integration/mysql_shared_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Shared MySQL' do
+describe 'Shared multi-tenant MySQL' do
   it 'can provision a service instance', components: [:ccng, :mysql] do
     ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 0
     expect {
@@ -8,5 +8,29 @@ describe 'Shared MySQL' do
       provision_mysql_instance("oursql")
     }.to change { mysql_root_connection["SHOW DATABASES"].to_a.size }.by(2)
     ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 2
+  end
+
+  let(:create_app_request) do
+    {
+        "space_guid" => space_guid,
+        "name" => "mysql_binding_test",
+        "instances" => 1,
+        "memory" => 256
+    }
+  end
+  it "can bind a service instance", components: [:ccng, :mysql] do
+    instance_guid = provision_mysql_instance("yoursql")
+    app_guid = ccng_post("/v2/apps", create_app_request).fetch("metadata").fetch("guid")
+
+    create_binding_request = {
+        app_guid: app_guid, service_instance_guid: instance_guid
+    }
+    creds = ccng_post("/v2/service_bindings", create_binding_request).fetch("entity").fetch("credentials")
+    conn_string = "mysql2://#{creds["user"]}:#{creds["password"]}@#{creds["host"]}:#{creds["port"]}/#{creds["name"]}"
+    Sequel.connect(conn_string) do |conn|
+      expect {
+        conn.run("CREATE TABLE ponies(hay INTEGER)")
+      }.to change{ conn["SHOW TABLES"].to_a.length }.from(0).to(1)
+    end
   end
 end
