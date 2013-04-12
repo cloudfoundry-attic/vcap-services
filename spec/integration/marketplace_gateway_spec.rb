99b78a227faab298c1d484b2558c8185e25f91ce
Jesse Zhang and Phan Le <pair+jesse+phan@pivotallabs.com>
2013-04-12 14:25:27 -0700

    integration test for marketplace services and plans

--- spec/integration/marketplace_gateway_spec.rb
+++ spec/integration/marketplace_gateway_spec.rb
@@ -12,23 +12,38 @@ describe 'Marketplace Gateway - AppDirect integration' do
   it 'populates CC with AppDirect services', components: [:ccng, :marketplace]  do
     services_response = nil
     # retry a few times, since the service may not advertise offerings immediately
-    10.times do
+    20.times do
       services_response = ccng_get('/v2/services')
       break if services_response.fetch('resources').any?
       sleep 0.5
     end
 
     services_response.fetch('resources').should have(1).entry
-    entity = services_response.fetch('resources').first.fetch('entity')
-    entity.fetch('label').should == 'mongodb'
-    entity.fetch('provider').should == 'mongolab'
+    service = services_response.fetch('resources').first.fetch('entity')
+    service.fetch('label').should == 'mongodb'
+    service.fetch('provider').should == 'mongolab'
 
-    entity.fetch('extra').should_not be_nil
-    extra_information = JSON.parse(entity.fetch('extra'))
+    service.fetch('extra').should_not be_nil
+    extra_information = JSON.parse(service.fetch('extra'))
+
     extra_information.fetch('provider').fetch('name').should == 'mongolab'
     extra_information.fetch('listing').fetch('imageUrl').should == "https://example.com/profileImageUrl"
     extra_information.fetch('listing').fetch('blurb').should == "MongoDB is WEB SCALE"
 
-    # pending 'checking service plans'
+    plans_url = service.fetch("service_plans_url")
+
+    plans = nil
+    10.times do
+      plans = ccng_get(plans_url)
+      break if plans.fetch('resources').any?
+      sleep 0.5
+    end
+
+    plans.fetch("total_results").should eq(2)
+    plan_names = plans.fetch("resources").map {|r| r.fetch("entity").fetch("name")}
+    plan_names.should match_array([
+      "free",
+      "small",
+    ])
   end
 end
