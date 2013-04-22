98e72f5cb80374ba8400723bd143990115d0807f
Jeff Schnitzer and Matt Royal <pair+jschnitzer+mroyal@pivotallabs.com>
2013-04-22 16:35:15 -0700

    Add integration test for deleting mysql database instances
    [Finishes #47406835]

--- spec/integration/mysql_shared_spec.rb
+++ spec/integration/mysql_shared_spec.rb
@@ -1,21 +1,12 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'Shared multi-tenant MySQL' do
-  it 'can provision a service instance', components: [:ccng, :mysql] do
-    ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 0
-    expect {
-      provision_mysql_instance("yoursql")
-      provision_mysql_instance("oursql")
-    }.to change { mysql_root_connection["SHOW DATABASES"].to_a.size }.by(2)
-    ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 2
-  end
-
+describe "Shared multi-tenant MySQL", components: [:ccng, :mysql] do
   let(:create_app_request) do
     {
-        "space_guid" => space_guid,
-        "name" => "mysql_binding_test",
-        "instances" => 1,
-        "memory" => 256
+      "space_guid" => space_guid,
+      "name"       => "mysql_binding_test",
+      "instances"  => 1,
+      "memory"     => 256
     }
   end
 
@@ -24,7 +15,7 @@ describe 'Shared multi-tenant MySQL' do
     app_guid = ccng_post("/v2/apps", create_app_request).fetch("metadata").fetch("guid")
 
     create_binding_request = {
-        app_guid: app_guid, service_instance_guid: instance_guid
+      app_guid: app_guid, service_instance_guid: instance_guid
     }
     ccng_post("/v2/service_bindings", create_binding_request)
   end
@@ -34,7 +25,16 @@ describe 'Shared multi-tenant MySQL' do
     "mysql2://#{creds["user"]}:#{creds["password"]}@#{creds["host"]}:#{creds["port"]}/#{creds["name"]}"
   end
 
-  it "can bind a service instance", components: [:ccng, :mysql] do
+  it "can provision a service instance" do
+    ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 0
+    expect {
+      provision_mysql_instance("yoursql")
+      provision_mysql_instance("oursql")
+    }.to change { mysql_root_connection["SHOW DATABASES"].to_a.size }.by(2)
+    ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 2
+  end
+
+  it "can bind a service instance" do
     conn_string = get_creds(bind_service)
     Sequel.connect(conn_string) do |conn|
       expect {
@@ -43,7 +43,7 @@ describe 'Shared multi-tenant MySQL' do
     end
   end
 
-  it "can unbind a service instance", components: [:ccng, :mysql] do
+  it "can unbind a service instance" do
     binding_response = bind_service
     ccng_delete("/v2/service_bindings/#{binding_response.fetch("metadata").fetch("guid")}")
     conn_string = get_creds(binding_response)
@@ -52,5 +52,12 @@ describe 'Shared multi-tenant MySQL' do
         conn.run("CREATE TABLE ponies(hay INTEGER)")
       end
     }.to raise_error(Sequel::DatabaseConnectionError, /Access denied/)
+  end
+
+  it "can delete a service instance" do
+    service_instance_guid = provision_mysql_instance("oursql")
+    ccng_delete("/v2/service_instances/#{service_instance_guid}")
+
+    ccng_get("/v2/spaces/#{space_guid}/service_instances").fetch("total_results").should == 0
   end
 end
