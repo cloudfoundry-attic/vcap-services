777c46a0ee88346fd2e4601a3b2451fd66ce502a
Jesse Zhang and Matt Royal <pair+jesse+mroyal@pivotallabs.com>
2013-05-23 11:53:54 -0700

    Use integration-test-support submodule for component runners

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,17 @@ ASSETS_DIR = File.join(SPEC_ROOT, "assets")
 BOSH_ROOT_DIR = File.expand_path File.join(SPEC_ROOT, "..")
 BOSH_TMP_DIR = File.expand_path File.join(BOSH_ROOT_DIR, "tmp")
 
-Dir.glob("#{SPEC_ROOT}/support/**/*.rb") do |filename|
-  require filename
+def require_dir(dir_pattern)
+  Dir.glob(File.expand_path(dir_pattern, File.dirname(__FILE__))) do |filename|
+    require filename
+  end
 end
+
+require_dir '../vendor/integration-test-support/support/**/*.rb'
+
+tmp_dir = File.expand_path('tmp', File.dirname(__FILE__))
+FileUtils.mkdir_p(tmp_dir)
+IntegrationExampleGroup.tmp_dir = tmp_dir
 
 RSpec.configure do |c|
   c.include IntegrationExampleGroup, :type => :integration, :example_group => {:file_path => /\/integration\//}
